e6d4b547d0cb6666077a23be6f34dd848de47cd4
purepennons <y80811@gmail.com>
2014-02-22 19:14:00 +0800

    Current date display , OK!

--- ECA_Checkin_System/js/action.js
+++ ECA_Checkin_System/js/action.js
@@ -3,10 +3,15 @@ var clockStart = setInterval(function(){getCurTime()},1000);
 
 function getCurTime() {
 	var systemTime = document.getElementById('system-time');
+	var systemDate = document.getElementById('system-date');
 	var currentTime = new Date();
 	var currentHours = currentTime.getHours();
 	var currentMins  = currentTime.getMinutes();
 	var currentSec = currentTime.getSeconds();
+	var monthName = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+	var year = currentTime.getFullYear();
+	var month = monthName[currentTime.getMonth()];
+	var date = currentTime.getDate();
 	var m = 'AM';
 	if(currentHours >= 12){
 		m = 'PM';
@@ -15,7 +20,9 @@ function getCurTime() {
 	currentHours = (currentHours / 10) > 1 ? currentHours : '0' + currentHours.toString();
 	currentMins = (currentMins / 10) > 1 ? currentMins : '0' + currentMins.toString();
 	currentSec = (currentSec / 10) > 1 ? currentSec : '0' + currentSec.toString();
+	date = (date / 10) > 1 ? date : '0' + date.toString();
 	//var t = currentTime.toLocaleTimeString();
  	//systemTime.innerHTML = t;
  	systemTime.innerHTML = m + ' ' + currentHours + ':' + currentMins + ':' + currentSec;
+ 	systemDate.innerHTML = month + ' - ' + date + ', ' + year;
 }
--- ECA_Checkin_System/php/general/GeneralUserIndex.php
+++ ECA_Checkin_System/php/general/GeneralUserIndex.php
@@ -66,7 +66,8 @@
 			<div class="jumbotron">
 				<div class="row">
 					<div class="col-md-8">
-	  				<h1 id="system-time"></h1>
+						<h2 id="system-date"></h2>
+	  					<h1 id="system-time"></h1>
 					</div>
   					<div class="col-md-4">
 	  					<p><a class="btn btn-primary btn-lg btn-block pull-right" role="button">Check in</a></p>
