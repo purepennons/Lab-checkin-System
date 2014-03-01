236c22e055db05a0cd51c22a131935ded57ec1ec
purepennons <y80811@gmail.com>
2014-03-01 16:55:28 +0800

    adminIndex - week-record display , ok! But data not display correctly.

--- ECA_Checkin_System/php/admin/adminQuery.php
+++ ECA_Checkin_System/php/admin/adminQuery.php
@@ -1,6 +1,8 @@
 <?php
 	$chooseDate = $_POST['datepicker'];
+	$chooseUsername = $_POST['select-option-name']
 	$_SESSION['sessiondate'] = $chooseDate;
+	$_SESSION['sessionChooseUsername'] = $chooseUsername;
 	echo "<script>document.location.href='AdminIndex.php'</script>";
 	exit;
 ?>
